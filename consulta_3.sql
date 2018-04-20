2d6123581cc0369d6bc101cdbe726df56a6ef513
Pedro Elpidio Melha Lemos <pedroemlemos@gmail.com>
2018-04-20 13:01:58 -0300

    Terceira consulta: não funcional ainda

--- consulta_3.sql
+++ consulta_3.sql
@@ -5,3 +5,10 @@
 --      - Seleções sobre colunas que não pertençam às chaves primárias das tabelas;
 --      - Ordenar a saída por coluna diferente de PRIMARY KEY de alguma tabela.
 -- - Cenário sugerido de otimização: transformação de junção externa em junção interna mais UNION com o que faltou.
+
+select NOME_RUBRICA, CODIGO_RUBRICA,    -- Colunas existentes em RUBRICA
+       VALOR_EMPENHADO, VALOR_PAGO      -- Colunas existentes em DESPESAS1
+from RUBRICAS left join DESPESAS1;      -- NÃO está funcionando...
+
+select * from RUBRICAS inner join DESPESAS1
+-- union Como seleciono os registros que não são em comum entre as duas tabelas? É a junção externa?;
